cce53e720f5cfe5bc181664198b69f70cd96f06f
Zeyad Sayed <zeyad67sayed@gmail.com>
2023-11-07 17:44:53 +0200

    change front & back end urls

--- src/constants/backend.ts
+++ src/constants/backend.ts
@@ -1,3 +1,4 @@
-const BACKEND_URL = "http://localhost:8080";
+// const BACKEND_URL = "http://localhost:8080";
+const BACKEND_URL = "https://amazon-clone-server-1nmg.onrender.com";
 
 export default BACKEND_URL;
